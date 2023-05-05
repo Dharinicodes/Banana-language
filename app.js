ca31336256873756d3860caa47e7aa6f22b6028c
Dharinicodes <126567741+Dharinicodes@users.noreply.github.com>
2023-05-05 13:02:01 +0530

    style: theme added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var txtOutput = document.querySelector("#txt-output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
@@ -12,7 +12,7 @@ function getTranslationURL(text) {
 function errorHandler(error) {
   console.log("error occured", error);
   alert(
-    "Something went wrong with the server! Please try again after sometime later"
+    "Something went wrong with the server! Please try again after sometime"
   );
 }
 
@@ -24,7 +24,7 @@ function clickHandler() {
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText; // to give output
+      txtOutput.innerText = translatedText; // to give output
     })
     .catch(errorHandler);
 }
